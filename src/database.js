fa3c05cb8a485dc7b62e0e491a717b968170800b
LuisDanielCL1996 <70722061+LuisDanielCL1996@users.noreply.github.com>
2020-09-30 12:23:25 -0500

    se añadio mongoDB

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/proyect-db-app',{
+mongoose.connect('mongodb+srv://Luis:abuelita91.@cluster0.i3u6z.mongodb.net/<dbname>?retryWrites=true&w=majority',{
 	useCreateIndex: true,
 	useNewUrlParser:true, //configuraciones basicas de conexion para no tener errores
 	useFindAndModify: false,  //Al momento de manipular la DB
